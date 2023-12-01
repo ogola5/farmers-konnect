61e93a9629b95fc602ad3fe42e227cb381cfd3af
ogola5 <ogolaevance5@.com>
2023-12-01 11:56:48 +0300

    enabled connection

--- frontend/src/components/pages/RegisterPage.js
+++ frontend/src/components/pages/RegisterPage.js
@@ -1,71 +1,54 @@
 // RegisterPage.js
-import React, { useState,useEffect } from 'react';
+import React, { useState } from 'react';
 import { ethers } from 'ethers';
 import InvestorFarmerRegistration from '../../contracts/InvestorFarmerRegistration.json';
 import contractAddress from '../../contracts/contract-address.json';
 
-// The local node URL; default for Hardhat is 'http://localhost:8545'
-const localNodeUrl = 'http://localhost:8545';
-const provider = new ethers.providers.JsonRpcProvider(localNodeUrl);
+
 
 const RegisterPage = () => {
     const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [profile, setProfile] = useState(null);
+    const [email, setEmail] = useState('');    
     const [userType, setUserType] = useState('Investor'); // or 'Farmer'
 
-    // Initialize ethers.js contract instance
-    const signer = provider.getSigner();
-    const registrationContract = new ethers.Contract(
-        contractAddress.InvestorFarmerRegistration,
-        InvestorFarmerRegistration.abi,
-        signer
-    );
-    // Get profile details for the connected account
-    const fetchProfile = async () => {
-        try {
-            // Request account access if needed (this will prompt MetaMask to connect)
-            const accounts = await provider.send("eth_requestAccounts", []);
-            const account = accounts[0]; // Use the connected MetaMask account
-
-            // Call the getProfile function from the contract
-            const profileData = await registrationContract.getProfile(account);
-            setProfile(profileData);
-        } catch (error) {
-            console.error(error);
-            alert('An error occurred while fetching the profile.');
+    
+    const requestAccount = async () => {
+        if (window.ethereum) {
+            try {
+                const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+                return accounts[0];
+            } catch (error) {
+                throw new Error('No account access granted by user.');
+            }
+        } else {
+            throw new Error('MetaMask is not installed.');
         }
     };
+    
 
     const handleRegister = async (e) => {
         e.preventDefault();
     
         try {
-            // Request account access if needed (this will prompt MetaMask to connect)
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
-    
-            // You now have access to the user's accounts
-            const accounts = await provider.listAccounts();
-            const account = accounts[0]; // Use the connected MetaMask account
-    
-            // Reconnect the signer with the MetaMask account
+            const account = await requestAccount(); // Request MetaMask account access
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
             const signer = provider.getSigner(account);
-            const registrationContractWithSigner = registrationContract.connect(signer);
+            const registrationContract = new ethers.Contract(
+                contractAddress.InvestorFarmerRegistration,
+                InvestorFarmerRegistration.abi,
+                signer
+            );
     
-            // Assuming the contract has a registerProfile function
-            const tx = await registrationContractWithSigner.registerProfile(name, email, userType);
+            const userTypeEnum = userType === 'Investor' ? 0 : 1; // Map user type to enum value
+            const tx = await registrationContract.registerProfile(name, email, userTypeEnum);
             await tx.wait();
     
             alert('Registration successful!');
         } catch (error) {
             console.error(error);
-            alert('An error occurred during registration.');
+            alert(`An error occurred during registration: ${error.message}`);
         }
     };
-    // Load the profile on component mount
-    useEffect(() => {
-        fetchProfile();
-    }, []);
     
     
 
@@ -93,15 +76,7 @@ const RegisterPage = () => {
                 </select>
                 <button type="submit">Register</button>
             </form>
-            <button onClick={fetchProfile}>Fetch Profile</button>
-            {profile && (
-                <div>
-                    <p>Name: {profile.name}</p>
-                    <p>Email: {profile.email}</p>
-                    <p>User Type: {profile.userType === 0 ? 'Investor' : 'Farmer'}</p>
-                    <p>Status: {profile.isActive ? 'Active' : 'Inactive'}</p>
-                </div>
-            )}
+            
         </div>
     );
 };
